010ffbee4537975af676921d304abad187af987b
j3102nnl <j3102nnl@sit.ac.jp>
2025-11-26 14:45:53 +0900

    異常値の追加

---
+++ test.bash
@@ -0,0 +1,17 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2022 Ryuichi Ueda
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo NG at Line $1
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+
+[ "${out}" = 15.0 ] || ng ${LINENO}
+
+[ "${res}" = 0 ] && echo OK
+exit $res
