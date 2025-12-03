f600dd0b681c762be862c7d4d233e7d6e8e503b2
j3102nnl <j3102nnl@sit.ac.jp>
2025-12-03 14:34:04 +0900

    fix test script

--- test.bash
+++ test.bash
@@ -1,25 +1,24 @@
-#!/bin/bash -xv
+#!/bin/bash
 # SPDX-FileCopyrightText: 2022 Ryuichi Ueda
 # SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
-	echo NG at Line $1
-	res=1
+    echo NG at Line $1
+    res=1
 }
 
 res=0
 
 out=$(seq 5 | ./plus)
+[ "${out}" = "15.0" ] || ng ${LINENO}
 
-[ "${out}" = 15.0 ] || ng ${LINENO}
-
-out=$(echo あ| ./plus)
-[ "$?" =1 ] || ng ${LINENO}
-[ "${out}" = "" ] ||ng ${LINENO}
+out=$(echo あ | ./plus)
+[ "$?" -eq 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
 out=$(echo | ./plus)
-[ "$?" = 1 ] ||ng ${LINENO}
-[ "${out}" = "" ] ||${LINENO}
+[ "$?" -eq 1 ] || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
 
-[ "${res}" = 0 ] && echo OK
+[ "${res}" -eq 0 ] && echo OK
 exit $res
